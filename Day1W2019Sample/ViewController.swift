0720c06ac585153343aad240ebbf10ff84063b8b
MacStudent <macstudent@126-iMac04.local>
2019-03-04 20:19:51 -0500

    INTRO

--- Day1W2019Sample/ViewController.swift
+++ Day1W2019Sample/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblProgram: UILabel!
+    @IBOutlet weak var lblMessage: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.lblMessage.text = "HELLO WORLD"
     }
 
 
